1326e46e598e9c6eff52de1da703303b01b8d7a4
Michal Vyskocil <michal.vyskocil@gmail.com>
2017-07-22 20:22:25 +0200

    Problem: tests together with app code
    Solution: move to own folder

--- tests/test_zm_rest.py
+++ tests/test_zm_rest.py
@@ -1,21 +1,4 @@
-# There will be the test of zm_rest
+from flask import url_for
 
-import pytest
-from collections import namedtuple
-
-import zm_rest
-
-@pytest.fixture (scope="module")
-def ctx ():
-    zm_rest.app.testing = True
-    Ctx = namedtuple ("Ctx", "app")
-    ctx = Ctx (zm_rest.app.test_client ())
-    with zm_rest.app.app_context ():
-        zm_rest.zm_rest.mlm_server ()
-        zm_rest.zm_rest.devices_actor ()
-        zm_rest.zm_rest.mlm_connect ()
-    yield ctx
-
-def test_slash (ctx):
-    rv = ctx.app.get (b"/")
-    assert b'["/devices"]' == rv.data
+def test_app (client):
+    assert client.get (url_for ("devices")).status_code == 200
--- zm_rest/test_zm_rest.py
+++
@@ -1,4 +0,0 @@
-from flask import url_for
-
-def test_app (client):
-    assert client.get (url_for ("devices")).status_code == 200
